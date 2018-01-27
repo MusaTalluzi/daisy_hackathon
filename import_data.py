b8ef5fe4916e8cff77a6bfa970b26797ca111e3b
Elliot Ford <elliot.ford@mail.utoronto.ca>
2018-01-27 12:07:38 -0500

    initial commit of module import_data.py

---
+++ import_data.py
@@ -0,0 +1,19 @@
+""" Reads in the dataFiles"""
+import pandas as pd
+import glob
+
+'''
+    takes in a regular expression describing the filepath to
+     the data files and returns a pandas dataFrame
+     
+     Usage: var_name = import_data("./hackathon_data/*20*.dat
+'''
+def import_data(file_regex):
+    all_files = glob.glob(file_regex)
+    ret = pd.DataFrame
+    list_ = []
+    for file_ in all_files:
+        df = pd.read_csv(file_)
+        list_.append(df)
+    ret = pd.concat(list_)
+    return ret
