2f41f9c101dd2377b350472c760be6a96efb3915
Elliot Ford <elliot.ford@mail.utoronto.ca>
2018-01-27 15:31:30 -0500

    Added the ability to index and format index by date. Added a usage example.

--- import_data.py
+++ import_data.py
@@ -2,18 +2,33 @@
 import pandas as pd
 import glob
 
-'''
-    takes in a regular expression describing the filepath to
-     the data files and returns a pandas dataFrame
-     
-     Usage:      
-     var_name = import_data.import_data("./hackathon_data/*20*.dat")
-'''
-def import_data(file_regex):
+
+def import_data(file_regex, index_col_val=None,
+                format_str=None):
+    """
+        takes in a regular expression describing the filepath to
+         the data files and returns a pandas dataFrame
+
+         Usage1:      
+         var_name = import_data.import_data("./hackathon_data/*20*.dat")
+         
+         Usage2:
+         var_name = import_data.import_data("./hackathon_data/*20*.dat",
+          "column of dates", "format of dates")
+
+    """
     all_files = glob.glob(file_regex)
+    all_files.sort()
     list_ = []
     for file_ in all_files:
-        df = pd.read_csv(file_)
+        if index_col_val is not None:
+            df = pd.read_csv(file_, index_col=index_col_val)
+        else:
+            df = pd.read_csv(file_)
         list_.append(df)
     ret = pd.concat(list_)
+    if index_col_val is not None and format_str is not None:
+        pd.to_datetime(ret.index, format=format_str,
+                       errors='ignore')
+        ret.sort_index(axis=0)
     return ret
